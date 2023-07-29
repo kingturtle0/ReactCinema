ad373026602689bb5b1029c1f6ad64501acdc01c
kingturtle0 <dhdnqkrtk22@naver.com>
2023-07-29 20:06:50 +0900

    Practice useEffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [value, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'value' changes.");
-  }, [value]);
-  useEffect(() => {
-    console.log("I run when keyword & value change");
-  }, [keyword, value]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type='text'
-        placeholder='Search here...'
-      />
-      <h1>{value}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
